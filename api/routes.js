ab7e5316e3eb4df891159d23f19397f2bb8865eb
jittawee <jtwe.esr@gmail.com>
2025-09-12 00:03:45 +0700

    -

--- api/routes.js
+++ api/routes.js
@@ -1,12 +1,14 @@
 import express from "express";
 import authRoutes from "./authRoutes.js";
-import { getAllUsers } from "./controllers/authcontroller.js";
+import { getAllUsers } from "./controllers/authController.js";
 import productsRoutes from "./productRoutes.js";
+import orderRoutes from "./orderRoutes.js";
 
 const router = express.Router();
 
 router.use("/products", productsRoutes);
 router.get("/users", getAllUsers);
 router.use("/auth", authRoutes);
+router.use("/orders", orderRoutes);
 
 export default router;
