c0ef9f65fda470cc5da8650a50d620671282037e
Supanat-pra <earthpra06@gmail.com>
2025-09-12 10:07:06 +0700

    delete user.js

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
 
 const UserSchema = new Schema(
   {
@@ -14,8 +15,16 @@ const UserSchema = new Schema(
     phone: { type: String, required: true, trim: true },
     username: { type: String, required: true, unique: true, trim: true },
     password: { type: String, required: true },
+    role: { type: String, enum:["user","admin"], default: "user",}
   },
   { timestamps: true }
 );
+
+// Hash password before saving
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 export const User = model("User", UserSchema);
